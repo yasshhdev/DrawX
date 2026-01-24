a714e8029415b71b811a17e70b8b6fd256d05ea1
yash <yashmali85540@gmail.com>
2026-01-24 16:32:03 +0530

    feat(room-chat):store user and room data in db properly

--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -1,8 +1,11 @@
 import jwt, { JwtPayload } from "jsonwebtoken"
-import { WebSocketServer } from "ws";
+import { WebSocketServer , WebSocket} from "ws";
 import "dotenv/config"
 import {getJwtSecret} from "@repo/backend-common/config"
+import {prismaclient} from "@repo/db/schema"
 
+
+// abt to add try catch blocks
 
 const JWT_SECRET = getJwtSecret();
 console.log(JWT_SECRET)
@@ -13,21 +16,41 @@ const rooms: Record<string,WebSocket[]> = {};
 const ws = new WebSocketServer({port:3030})
 
 
-function userauth(token:string):string|null{
+function userauth(token:string):number|null{
 
       const payload  =  jwt.verify(token,JWT_SECRET)
       
       if(typeof payload == "string" ) {return null}
     
-      if(!payload || !payload.userid) {return null}
+      if(!payload || !payload.userId) {return null}
 
-      return payload.userid;
+      return payload.userId;
 
 }
 
+async  function createrom(currentroom:string,userId:number){
+
+    const add = await prismaclient.rooms.create({
+        data:{
+                roomname:currentroom, 
+                admin:userId
+
+        }
+    })
+
+}
+
+
+
+
 ws.on("connection",(socket,requesturl)=>{
 
+
+    console.log("an user connnected")
+
     // if sending token in url only 
+    let currentroom :string | null = null 
+    let username :string | null= null 
      
     if(!requesturl){
         return;
@@ -37,22 +60,72 @@ ws.on("connection",(socket,requesturl)=>{
     const queryParams =new URLSearchParams(url?.split("?")[1])
     const token = queryParams.get("token") || "";
   
-     const userid = userauth(token);
-     if(!userid){socket.close;}
+     const userId = userauth(token);
+     if(!userId){socket.close();}
+
 
 
     socket.on("message",msg=>{
 
+    const data = JSON.parse(msg.toString())
+
     // alternative - if we send token on "socket.onopen" from fe
     
-    // const data = JSON.parse(msg.toString())
    
     // if(data.type === "auth"){
     //     const token = userauth(data.token)
     // }
 
+    
+
+    if(data.type==="join"){
+        currentroom = data.room;
+
+        if(!currentroom) {return}
+        
+        if(!rooms[currentroom]){
+            rooms[currentroom] = []
+
+           if(userId===null){return}
+            const newroom = createrom(currentroom,userId)
+        }
+
+        rooms[currentroom]?.push(socket)
+
+        username= data.username;
+
+        // const useradd =adduser()
+
+
+
+
+
+        console.log("room created successflly")
+
+
+    }
+
+    if (data.type==="chat"){
+        if(!currentroom) {return}
+
+        rooms[currentroom]?.forEach(user => {
+            if(user.readyState == WebSocket.OPEN) {
+                user.send(JSON.stringify({
+                    text:data.text,
+                    room:currentroom, 
+                    sender:username
+                }))
+            }
+        });
+    }
+
+    
 
     })
+
+     socket.on("close",()=>{console.log("An client disconnected")})
+
+
 
 })
 
@@ -69,84 +142,3 @@ ws.on("connection",(socket,requesturl)=>{
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// let rooms : Record<string,WebSocket[]>={}
-
-// const wss = new WebSocketServer({port:4000});
-
-// wss.on("connection",(socket,req)=>{
-
-//     const url = req.url;
-//     const queryParam = new URLSearchParams(url?.split('?')[1])
-//     const token = queryParam.get("token")??""
-
-//     const verify = jwt.verify(token,JWT_SECRET!) // payload | strign 
-    
-//     if(typeof(verify)=="string"){socket.close; return} // we need payload 
-
-//     if(!verify || !verify.userid){
-//         socket.close;
-//         return;
-//     }
-
-//     const userid = verify.userid;
-//     let currentroom : string | null | undefined = null ; 
-//     let username : string | null  = null; 
-
-//      socket.on ("message",data=>{
-
-//         let msg = JSON.parse(data.toString())
-
-//         if(msg.type=== "join")
-//         {
-//             currentroom = msg.room ;
-
-//             if(!currentroom) {return}
-
-//             if(!rooms[currentroom]) {rooms[currentroom]=[]}
-            
-//             rooms[currentroom]?.push(socket)
-
-//             username = msg.username;
-//             console.log("Joined successfully")
-//         }
-
-//         if(msg.type === "chat"){
-
-//             if(!currentroom){return}
-
-//             rooms[currentroom]?.forEach((c)=>{
-//                 if(c.readyState == WebSocket.OPEN)
-//                 {
-//                     c.send(JSON.stringify({
-//                         text:msg.text,
-//                         room:currentroom,
-//                         sender : username 
-//                     }))
-//                 }
-//             })
-//         }
-
-//     })
-
-//     socket.on("close",()=>{console.log("An client disconnected")})
-
-
-
-
-// })
